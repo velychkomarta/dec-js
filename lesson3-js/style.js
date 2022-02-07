f2e2e4d0a75bbedf5676960e85a3dba5f7c21591
velychkomarta <velychko0511@gmail.com>
2022-02-07 09:55:40 +0200

    js home

---
+++ lesson3-js/style.js
@@ -0,0 +1,105 @@
+// зробити масив з 10 чисел [2,17,13,6,22,31,45,66,100,-18]та:
+// 1. перебрати його циклом while
+// let mass1 = [2,17,13,6,22,31,45,66,100,-18];
+// let i = 0;
+// while (i<mass1.length){
+//     console.log(mass1[i]);
+//     i++;
+// }
+
+// 2. перебрати його циклом for
+
+// let mass2 = [2,17,13,6,22,31,45,66,100,-18];
+// for (let i=0; i<mass2.length; i++){
+//     console.log(mass2[i]);
+// }
+
+// 3. перебрати циклом while та вивести  числа тільки з непарним індексом
+
+// let mass3 = [2,17,13,6,22,31,45,66,100,-18];
+// let i =0;
+// while (i<mass3.length){
+//       i++;
+//       if (mass3[i]%2) console.log(mass3[i]);
+// }
+
+// 4. перебрати циклом for та вивести  числа тільки з непарним індексом
+
+// let mass4 = [2,17,13,6,22,31,45,66,100,-18];
+// for (let i=0; i<mass4.length; i++){
+//     if (mass4[i]%2 == 1) console.log(mass4[i]);
+// }
+
+// 5. перебрати циклом while та вивести  числа тільки парні  значення
+
+// let mass5 = [2,17,13,6,22,31,45,66,100,-18];
+// let i = 0;
+// while (i<mass5.length) {
+//     i++;
+//     if (mass5[i] % 2 === 0) console.log(mass5[i]);
+// }
+
+// 6. перебрати циклом for та вивести  числа тільки парні  значення
+
+// let mass6 = [2,17,13,6,22,31,45,66,100,-18];
+// for ( let i=0; i<mass6.length; i++){
+//     if (mass6[i] % 2 == 0) console.log(mass6[i]);
+// }
+
+// 7. замінити кожне число кратне 3 на слово "okten"
+
+// let mass7 = [2,17,13,6,22,31,45,66,100,-18];
+// for (let i=0; i<mass7.length; i++){
+//     if (mass7[i] % 3 === 0)
+//         console.log(mass7[i] + ' ' + "okten");
+// }
+
+// 8. вивести масив в зворотньому порядку.
+
+// let mass8 = [2,17,13,6,22,31,45,66,100,-18];
+// for (i=mass8.length; i>-1; i--){
+//     console.log(mass8[i]);
+// }
+
+// 9. всі попередні завдання (окрім 8), але в зворотньому циклі (с заду на перед)
+
+// let mass1 = [2,17,13,6,22,31,45,66,100,-18];
+// let i = mass1.length;
+// while (i>-1){
+//     console.log(mass1[i]);
+//     i--;
+// }
+
+// let mass2 = [2,17,13,6,22,31,45,66,100,-18];
+// for (let i=mass2.length; i>-1; i--){
+//     console.log(mass2[i]);
+// }
+//
+// let mass3 = [2,17,13,6,22,31,45,66,100,-18];
+// i=mass3.length;
+// while (let i>-1){
+//       i--;
+//       if (mass3[i]%2) console.log(mass3[i]);
+// }
+
+// let mass4 = [2,17,13,6,22,31,45,66,100,-18];
+// for (let i=mass4.length; i>0; i--){
+//     if (mass4[i]%2 == 1) console.log(mass4[i]);
+// }
+
+// let mass5 = [2,17,13,6,22,31,45,66,100,-18];
+// let i=mass5.length;
+// while (i>0) {
+//     i--;
+//     if (mass5[i] % 2 === 0) console.log(mass5[i]);
+// }
+
+// let mass6 = [2,17,13,6,22,31,45,66,100,-18];
+// for (let i =  mass6.length; i>0; i--){
+//     if (mass6[i] % 2 == 0) console.log(mass6[i]);
+// }
+
+// let mass7 = [2,17,13,6,22,31,45,66,100,-18];
+// for ( let i = mass7.length; i>0; i--){
+//     if (mass7[i] % 3 == 0) console.log(mass7[i]);
+// }
