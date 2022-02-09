8aadb08adc826cc94439781c9ac3f1708fb87f7b
velychkomarta <velychko0511@gmail.com>
2022-02-09 12:48:53 +0200

    js home

--- lesson4-js/style.js
+++ lesson4-js/style.js
@@ -67,3 +67,66 @@
 // console.log(f6(massiveSum))
 
 // створити функцію яка приймає масив чисел та повертає середнє арифметичне його значень.
+//
+// let massiveMidle = [5, 44, 3, 57, 897, 1290, 11,1, 555];
+//
+// function f7(array) {
+//     let sum = 0;
+//     for (let newArray of array) {
+//         sum = newArray + sum;
+//
+//     }
+//     return sum / array.length;
+// }
+//
+// let result = f7(massiveMidle);
+// console.log(result);
+
+// створити функцію яка приймає будь-яку кількість чисел, повертає найменьше,
+//     а виводить найбільше (Math використовувати заборонено);
+// function f8(){
+//     let max = arguments[0];
+//     let min = arguments[0];
+//     for (const newArray of arguments) {
+//        if (newArray>max){
+//            max=newArray;
+//        }  if(newArray<min){
+//
+//            min = newArray;
+//        }
+//     }
+//     console.log(max);
+//     return min;
+// }
+// let minResult = f8(11,5,12,67,12);
+// // console.log(minResult);
+
+// створити функцію яка заповнює масив рандомними числами
+// (цей код генерує рандомні числа в діапазоні від 0 до 100 -
+// Math.round(Math.random()*100)) та виводить його.
+
+// function f9(){
+// return Math.floor(Math.random() * 100)+1;
+// }
+// let result = f9();
+// console.log(result);
+
+// створити функцію яка заповнює масив рандомними числами в діапазоні від 0 до limit.
+//     limit - аргумент, який характеризує кінцеве значення діапазону.
+
+// function f10(limit){
+//     return Math.floor(Math.random() * limit);
+// }
+// let result = f10(20);
+// console.log(result);
+
+// Функція приймає масив та робить з нього новий масив в зворотньому порядку. [1,2,3] -> [3, 2, 1].
+
+function  f11(array){
+    let go = array;
+    console.log(array);
+    let goBack = array.reverse();
+    console.log(goBack);
+    return goBack;
+}
+let result = f11([3,11,568,32,11]);
