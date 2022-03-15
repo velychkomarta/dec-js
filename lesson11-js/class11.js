698e6c392ac046d41147a12b86838dc45a30ab1f
velychkomarta <velychko0511@gmail.com>
2022-03-15 21:05:51 +0200

    js home

---
+++ lesson11-js/class11.js
@@ -0,0 +1,55 @@
+// є масив -
+// let users = [
+//     {name: 'vasya', age: 31, status: false},
+//     {name: 'petya', age: 30, status: true},
+//     {name: 'kolya', age: 29, status: true},
+//     {name: 'olya', age: 28, status: false},
+//     {name: 'max', age: 30, status: true},
+//     {name: 'anya', age: 31, status: false},
+//     {name: 'oleg', age: 28, status: false},
+//     {name: 'andrey', age: 29, status: true},
+//     {name: 'masha', age: 30, status: true},
+//     {name: 'olya', age: 31, status: false},
+//     {name: 'max', age: 31, status: true}
+// ];
+// створити під кожен об'єкт свій блок з конопкою "додати до улюблених" при натисканні на яку об'єкт потрапляє до масиву
+// favorites улюблених обраних об'єктів в локальному сховищі.
+// Створити сторніку favorites.html при переході на яку потрібно вивест в документ всіх обраних на попередньому етапі.
+let users = [
+    {name: 'vasya', age: 31, status: false},
+    {name: 'petya', age: 30, status: true},
+    {name: 'kolya', age: 29, status: true},
+    {name: 'olya', age: 28, status: false},
+    {name: 'max', age: 30, status: true},
+    {name: 'anya', age: 31, status: false},
+    {name: 'oleg', age: 28, status: false},
+    {name: 'andrey', age: 29, status: true},
+    {name: 'masha', age: 30, status: true},
+    {name: 'olya', age: 31, status: false},
+    {name: 'max', age: 31, status: true}
+];
+
+let key = 'key';
+localStorage.setItem(key, JSON.stringify([]));
+let container = document.createElement('div');
+container.classList.add('container');
+document.body.append(container);
+
+for (const usersKey of users) {
+
+    let divUser = document.createElement('div');
+    divUser.className = 'user';
+    let divContent = document.createElement('div')
+    divContent.innerHTML = `Name: ${usersKey.name} Age: ${usersKey.age} Status: ${usersKey.status}`
+    let btn = document.createElement('button');
+    btn.innerHTML = 'Add to favorites';
+    btn.onclick = function () {
+        let best = JSON.parse(localStorage.getItem(key))
+        best.push(usersKey);
+        localStorage.setItem(key, JSON.stringify(best));
+        btn.disabled = true;
+
+    }
+    divUser.append(divContent, btn);
+    container.append(divUser)
+}
